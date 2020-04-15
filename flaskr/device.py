515ff79bae1276ae4f52f3494424974bc4b32dc9
Ben Matheja <ben@matheja.me>
2020-04-15 17:28:08 +0200

    provide more device information for /list

--- flaskr/device.py
+++ flaskr/device.py
@@ -3,6 +3,7 @@ import soco
 import urllib
 
 from soco.discovery import by_name
+from collections import defaultdict
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for, jsonify
 )
@@ -13,12 +14,21 @@ bp = Blueprint('device', __name__, url_prefix='/devices')
 
 @bp.route('/list', methods=['GET'])
 def listAvailableDevices():
-    devices = []
+    d = {}
     for zone in soco.discover():
-        devices.append(zone.player_name)
-        print("Found " + zone.uid + " named " + zone.player_name + " at volume " +
-              str(zone.volume) + " which is coordinator? " + str(zone.is_coordinator))
-    return jsonify({'devices': devices}), 200
+        d[zone.player_name]={}
+        device = by_name(zone.player_name)
+        #devices.append(zone.player_name)
+        d[zone.player_name]['zone.player_name'] = zone.player_name
+        d[zone.player_name]['zone.uid'] = zone.uid
+        d[zone.player_name]['zone.volume'] = zone.volume
+        d[zone.player_name]['zone.is_coordinator'] = zone.is_coordinator
+        d[zone.player_name]['transport.state'] = device.get_current_transport_info()['current_transport_state']
+
+        #print("Found " + zone.uid + " named " + zone.player_name + " at volume " +
+        #     str(zone.volume) + " which is coordinator? " + str(zone.is_coordinator))
+    print(d)
+    return jsonify({'devices': d}), 200
 
 
 @bp.route('/inspect/<name>')
